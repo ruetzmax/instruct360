e61f5a476fe000dfb3baec90afe0556ca1d8d0f0
ruetzmax <max.ruetz02@gmail.com>
2026-02-10 10:55:11 +0100

    output 2d bounding boxes

--- src/tracking.py
+++ src/tracking.py
@@ -8,17 +8,17 @@ def get_bounding_boxes_for_class(image, class_name, threshold_2d=0.25, threshold
     
     image_chunks = bounding_boxes_to_image_chunks(image, bounding_boxes_2d)
     
-    all_bb_centers = []
-    all_bb_dimensions = []
-    all_bb_poses = []
+    all_3d_bb_centers = []
+    all_3d_bb_dimensions = []
+    all_3d_bb_poses = []
     for chunk in image_chunks:
-        bb_centers, bb_dimensions, bb_poses = get_3d_bounding_boxes(chunk, class_name, threshold=threshold_3d)
-        bb_centers_adjusted, bb_poses_adjusted = adjust_rotation_by_chunk_rotation(bb_centers, bb_poses, chunk)
-        all_bb_centers.extend(bb_centers_adjusted)
-        all_bb_dimensions.extend(bb_dimensions)
-        all_bb_poses.extend(bb_poses_adjusted)
+        bb_3d_centers, bb_3d_dimensions, bb_3d_poses = get_3d_bounding_boxes(chunk, class_name, threshold=threshold_3d)
+        bb_3d_centers_adjusted, bb_3d_poses_adjusted = adjust_rotation_by_chunk_rotation(bb_3d_centers, bb_3d_poses, chunk)
+        all_3d_bb_centers.extend(bb_3d_centers_adjusted)
+        all_3d_bb_dimensions.extend(bb_3d_dimensions)
+        all_3d_bb_poses.extend(bb_3d_poses_adjusted)
         
-    return all_bb_centers, all_bb_dimensions, all_bb_poses
+    return bounding_boxes_2d, all_3d_bb_centers, all_3d_bb_dimensions, all_3d_bb_poses
 
 def track_objects_in_video(classes, threshold_2d=0.25, threshold_3d=0.3, export_meshes=False, colors=None, video_path=None, left_video_path=None, right_video_path=None):
     
@@ -41,7 +41,7 @@ def track_objects_in_video(classes, threshold_2d=0.25, threshold_3d=0.3, export_
                 frame, class_name, threshold_2d=threshold_2d, threshold_3d=threshold_3d
             )
             
-            bb_centers, bb_dimensions, bb_poses = bbs
+            bb2ds, bb_centers, bb_dimensions, bb_poses = bbs
             
             if not bb_centers:
                 continue
@@ -49,6 +49,7 @@ def track_objects_in_video(classes, threshold_2d=0.25, threshold_3d=0.3, export_
             
             class_result = {
                 'class_name': class_name,
+                'bb2ds': bb2ds,
                 'centers': bb_centers,
                 'dimensions': bb_dimensions,
                 'poses': bb_poses
@@ -56,7 +57,7 @@ def track_objects_in_video(classes, threshold_2d=0.25, threshold_3d=0.3, export_
             
             if export_meshes:
                 color = colors[class_idx] if colors else get_color_by_index(class_idx)
-                bb_meshes = get_box_meshes(bbs, color=color)
+                bb_meshes = get_box_meshes((bb_centers, bb_dimensions, bb_poses), color=color)
                 bb_mesh_dicts = [mesh_to_dict(mesh) for mesh in bb_meshes]
                 class_result['meshes'] = bb_mesh_dicts
 
